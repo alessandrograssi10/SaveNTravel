b7da0b84399d83d752fddeba10a1baef134f2053
tequilasunrisecoder <grassi1981987@studenti.uniroma1.it>
2024-06-27 21:30:02 +0200

    Initial Commit

---
+++ SaveNTravel2/SaveNTravel2App.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNTravel2App.swift
+//  SaveNTravel2
+//
+//  Created by Davide Grassi on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SaveNTravel2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SaveNTravel2UITests/SaveNTravel2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SaveNTravel2UITestsLaunchTests.swift
+//  SaveNTravel2UITests
+//
+//  Created by Davide Grassi on 27/06/24.
+//
+
+import XCTest
+
+final class SaveNTravel2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
